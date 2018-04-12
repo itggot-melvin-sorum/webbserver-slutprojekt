b4602d92d675dca8fd5f1e79936ccb30d6999ba4
melvin.sorum <melvin.sorum@itggot.se>
2018-04-12 10:38:52 +0200

    Registring och inloggning nästan klart

--- app.rb
+++ app.rb
@@ -1,7 +1,27 @@
 class App < Sinatra::Base
 
+	enable :sessions
+
+	def set_error(error_message)
+		session[:error] = error_message
+	end
+
+	def get_error()
+		error = session[:error]
+		session[:error] = nil
+		return error
+	end
+
 	get '/' do
 		slim(:index)
+	end
+
+	get '/home' do
+		slim(:home)
+	end
+
+	get('/error') do
+		slim(:error)
 	end
 
 	get '/login' do
@@ -10,6 +30,25 @@ class App < Sinatra::Base
 
 	get '/register' do
 		slim(:register)
+	end
+
+	get '/computer_form' do
+		slim(:computer_form)
+	end
+
+	get '/my_computer' do
+		slim(:my_computer)
+	end
+
+	post 'computer_form' do
+		db = SQLite3::Database.new('db/database.db')
+		db.results_as_hash = true
+
+		budget = params["budget"]
+		computer_use = params["computer_use"]
+
+		result = db.execute("SELECT id FROM users WHERE username=?", [username])
+
 	end
 
 	post '/register' do
@@ -21,6 +60,44 @@ class App < Sinatra::Base
 		password_confirmation = params["confirm_password"]
 
 		result = db.execute("SELECT id FROM users WHERE username=?", [username])
+
+		if result.empty?
+			if password == password_confirmation
+				password_digest = BCrypt::Password.create(password)
+
+				db.execute("INSERT INTO users(username, password_digest) VALUES (?,?)", [username, password_digest])
+				redirect('/login')
+			else
+				set_error("Passwords don't match")
+				redirect('/error')
+			end
+		else
+			set_error("Username already exists")
+			redirect('/error')
+		end	
 	end
 
+	post '/login' do 
+		db = SQLite3::Database.new('db/database.db')
+		db.results_as_hash = true
+		username = params["username"]
+		password = params["password"]
+
+		result = db.execute("SELECT id, password_digest FROM users WHERE username=?", [username])
+
+		if result.empty?
+			set_error("Invalid Credentials")
+			redirect('/error')
+		end
+
+		user_id = result.first["id"]
+		password_digest = result.first["password_digest"]
+		if BCrypt::Password.new(password_digest) == password
+			session[:user_id] = user_id
+			redirect('/my_computer')
+		else 
+			set_error("Invalid Credentials")
+			redirect('/error')
+		end
+	end
 end           
